031d7347d1ad9bce286a870d5b3c1a76e05122f5
Aleksey Sviridkin <f@lex.la>
2018-04-27 02:17:25 +0300

    Add constructor

--- doctor/optimisation.go
+++ doctor/optimisation.go
@@ -4,9 +4,13 @@ import (
 	"github.com/lexfrei/lolnet"
 )
 
-var _ lolnet.Defibrillator = &rawBlood{}
+var _ lolnet.Doctor = &rawBlood{}
 
 type rawBlood struct {
+}
+
+func NewRawBlood() rawBlood {
+	return rawBlood{}
 }
 
 func (rawBlood) Diagnose(blood *lolnet.Blood) error {
