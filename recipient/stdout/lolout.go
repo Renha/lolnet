b5c53582352af74ad7efcec2aa8263cf293fcb6e
Aleksey Sviridkin <f@lex.la>
2018-04-27 13:31:00 +0300

    Add short and full output

--- recipient/stdout/lolout.go
+++ recipient/stdout/lolout.go
@@ -9,24 +9,39 @@ import (
 var _ lolnet.Recipient = &lolout{}
 
 type lolout struct {
+	short bool
 }
 
-// Newlolout provides new recipient object
-func Newlolout() lolout {
-	return struct{}{}
+// NewStdOut provides new recipient object
+func NewStdOut(short bool) *lolout {
+	return &lolout{
+		short: short,
+	}
 }
 
 func (lolout) Remove() error {
 	return nil
 }
 
-func (lolout) Add(bl *lolnet.Blood) error {
-	fmt.Println(bl.String())
+func (lo lolout) Add(bl *lolnet.Blood) error {
+	if lo.short {
+		fmt.Println(bl.String())
+	} else {
+		var out string
+		for _, i := range bl.IPs {
+			out += fmt.Sprintf("%s\n", i)
+		}
+		for _, o := range bl.Nets {
+			out += fmt.Sprintf("%s\n", o.String())
+		}
+		fmt.Println(out)
+	}
 	return nil
 }
 
-func (l lolout) Update(bl *lolnet.Blood) error {
-	l.Remove()
-	fmt.Println(bl.String())
-	return nil
+func (lo lolout) Update(bl *lolnet.Blood) error {
+	if err := lo.Remove(); err != nil {
+		return err
+	}
+	return lo.Add(bl)
 }
